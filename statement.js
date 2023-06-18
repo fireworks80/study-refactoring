46aa4df881b17832f4231d39b01281ec1616783f
fireworks <seungchulhan@gmail.com>
2023-06-18 22:45:47 +0900

    fix: totalVolumeCredits 함수 reduce로 변경

--- statement.js
+++ statement.js
@@ -49,11 +49,7 @@ export function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
+    return invoice.performances.reduce((acc, perf) => (acc += volumeCreditsFor(perf)), 0);
   }
 
   for (let perf of invoice.performances) {
