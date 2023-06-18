58b81f8c2cf5a426db19af17b034015ea816e306
fireworks <seungchulhan@gmail.com>
2023-06-18 22:31:50 +0900

    fix:for문에서 volumeCredits계산 부분 함수로 분리

--- statement.js
+++ statement.js
@@ -35,18 +35,21 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  for (let perf of invoice.performances) {
-    const thisAmount = amountFor(perf);
-
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
     // 청구 내역 출력
-    result += `${playFor(perf).type}: ${format(thisAmount / 100)}(${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).type}: ${format(amountFor(perf) / 100)}(${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   } // for
 
   result += `총액: ${format(totalAmount / 100)}\n`;
