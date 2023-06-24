2751d2b730834a6630cfe743a9c728fa0c9b7e52
fireworks <seungchulhan@gmail.com>
2023-06-24 14:59:59 +0900

    update: 다형성을활용하여 데이터 생성하기

--- createStatementData.js
+++ createStatementData.js
@@ -1,44 +1,54 @@
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-
-      case 'comedy': //희극
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    } // switch
-
-    return result;
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
   }
-  get volumeCredit() {
-    let result = 0;
-    // 포인트를 적립한다.
-    result += Math.max(this.performance.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
-
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+// 비극 클래스
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
     return result;
   }
 }
+
+// 희극 클래스
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+
+    result += 300 * this.performance.audience;
+
+    return result;
+  } // get
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+} // class
 
 export default function createStatementData(invoice, plays) {
   function playFor(aPerformance) {
@@ -50,13 +60,13 @@ export default function createStatementData(invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    return data.performances.reduce((acc, aPerformance) => (acc += aPerformance.volumeCredit), 0);
+    return data.performances.reduce((acc, aPerformance) => (acc += aPerformance.volumeCredits), 0);
   }
 
   const enrichPerformance = (aPerformance) => {
-    const { play, amount, volumeCredit } = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const { play, amount, volumeCredits } = createPerformanceCalculator(aPerformance, playFor(aPerformance));
 
-    return { ...structuredClone(aPerformance), play, amount, volumeCredit };
+    return { ...structuredClone(aPerformance), play, amount, volumeCredits };
   };
 
   const statementData = {};
